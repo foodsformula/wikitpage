66437c16dc36e7af894901819bf23362d90f1300
foodsformula <foodsformulas@gmail.com>
2013-04-22 09:00:04 +0800

    Update wikitpage.js

--- wikitpage.js
+++ wikitpage.js
@@ -1,5 +1,5 @@
 var config={
-  user:'peihsinsu', //The github owner or group name, that will use for build edit link
+  user:'foodsformula', //The github owner or group name, that will use for build edit link
   project:'wikitpage', //The github project name, that will use for build edit link
   md_file_path: '../mdfiles', //The md file folder that for read md file use
   welcome_page: 'Main.md', //The welcome page, default location that access from root url
